5b5ac73463db49a19fff89e2c2cbd7ba4316708f
santosh <santoshborate.mcs@gmail.com>
2017-08-19 01:35:36 +0530

    Post view design changed

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -2,34 +2,32 @@
 @section('title', 'Show Post')
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Read Post</h2>
-            </div>
-            <div class="pull-right">
-                <a href="{{ url('/post') }}" class="btn btn-info pull-right"> Back</a>
-            </div>
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            Read Post <a href="{{ url('/post') }}" class="label label-primary pull-right">Back</a>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Title:</strong>
-                {{ $post->name }}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Content:</strong>
-                {{ $post->content }}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Published On:</strong>
-                {{ $post->created_at }}
-            </div>
+        <div class="panel-body">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Title:</strong>
+                            {{ $post->name }}
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Content:</strong>
+                            {{ $post->content }}
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Published On:</strong>
+                            {{ $post->created_at }}
+                        </div>
+                    </div>
+                </div>
         </div>
     </div>
 @endsection
