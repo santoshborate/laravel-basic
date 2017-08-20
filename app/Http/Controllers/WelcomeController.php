b8a07aeb8eeb1358180243c59a9e3525acbde6d3
santosh <santoshborate.mcs@gmail.com>
2017-08-20 11:08:26 +0530

    Welcome controller added

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    /**
+     * Display dashboard.
+     *
+     */
+    public function index()
+    {
+        return view('welcome');
+    }
+}
